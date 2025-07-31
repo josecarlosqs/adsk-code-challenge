bbdc4ddb7c3cbc621593aa85268d09a28100e370
José Quichiz <josec.quichiz@gmail.com>
2025-07-31 12:56:19 -0500

    Route manager

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,8 +1,31 @@
 import type { Request, Response } from 'express';
+import { booksService } from '../services';
+import { matchedData, Result, validationResult } from 'express-validator';
 
-export const searchBooks = (req: Request, res: Response) => {
+export const searchBooks = async (req: Request, res: Response) => {
+  const validation: Result = validationResult(req),
+        validationArray = validation.array();
+
+  if (validationArray.length > 0) {
+    return res.status(400).json({
+      success: false,
+      message: validationArray.map((err) => {
+        return `${err.msg} for ${err.path} query param`;
+      })
+    })
+  }
+
+  const data = matchedData(req);
+
+  data.q = data.q.replace(/\s+/g, '+');
+
+  const result = await booksService.search(data.q, data.page);
+
   res.status(200).json({
     success: true,
-    data: [],
+    data: result.map(book => ({
+      title: book.title,
+      author: book.author
+    })),
   });
 };
