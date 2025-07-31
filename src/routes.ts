d1d6d47632235d0df0c35636a8b852e9e607b8eb
José Quichiz <josec.quichiz@gmail.com>
2025-07-31 12:54:38 -0500

    Added validation steps to route

--- src/routes.ts
+++ src/routes.ts
@@ -4,4 +4,4 @@ import { searchBooks } from './controllers/bookController';
 
 export const routes = Router();
 
-routes.get('/search', query('q').trim().escape().notEmpty(), searchBooks);
+routes.get('/search', query('q').trim().escape().notEmpty(), query('page').isNumeric().optional(), searchBooks);
